15de44840d26d4f2f60bec0f592f3dc923f560a1
Ubuntu <azureuser@ECI.byfa0imydz0upcig3tfp2epscb.fx.internal.cloudapp.net>
2023-03-22 16:53:26 +0000

    MAJ main_with_url

---
+++ main_with_url.py
@@ -0,0 +1,61 @@
+#-----------install librairies--------------
+
+import os
+import pandas as pd
+from datetime import datetime
+import random
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+from utils_radio import download_audio, transcript
+import warnings
+warnings.filterwarnings("ignore")
+
+#-----------PIPELINE-----------------------
+
+#DOWNLOAD AUDIO FROM URL, MAKE TRANSCRIPTION, DELETE AUDIO
+MODEL_ID = "jonatasgrosman/wav2vec2-large-xlsr-53-french"
+processor = Wav2Vec2Processor.from_pretrained(MODEL_ID)
+model = Wav2Vec2ForCTC.from_pretrained(MODEL_ID)
+
+df_trans = pd.read_csv("./transcript.csv", delimiter = ",", header = 0)
+df_url = pd.read_csv("./7-9_URLs.csv", delimiter = ",", header = 0)
+
+for i in range (32, 501):
+    i = i-32
+    if (i%5) == 0:
+        r = random.randint(0, 4)
+        i += r
+    else:
+        continue
+
+    audio_url = df_url["URL"][i]
+    date = str(audio_url[54:64])
+    date_obj = datetime.strptime(date, "%d.%m.%Y")
+    date = int(date_obj.timestamp())
+    id = i
+    titre = "inconnu"
+    if date in df_trans["Date"].values:
+        index_trans = df_trans.loc[df_trans["Date"] == date].index[0]
+        if not pd.isnull(df_trans.loc[index_trans, 'Transcript']):
+            continue
+    
+    #make transcription
+    print("transcription: start")
+    if str(id)+'.mp3' not in os.listdir('./audios'):
+        download_audio(str(id), str(audio_url)) #download in audios folder
+    audio_file = "./audios/"+str(id)+".mp3"
+    text = transcript(audio_file, processor, model)
+    
+    if date in df_trans["Date"].values:
+        index_trans = df_trans.loc[df_trans["Date"] == date].index[0]
+        df_trans.loc[index_trans, 'Transcript'] = text
+    else :
+        new_row = {'Id': id, 'Titre': titre, 'Date': date, 'Url': audio_url, 'Transcript': text}
+        df_trans = df_trans.append(new_row, ignore_index=True)
+    index_trans = df_trans.loc[df_trans["Date"] == date].index[0]
+    df_trans.to_csv('./transcript.csv', index=False)
+
+    #delete old audios
+    if (not pd.isnull(df_trans.loc[index_trans, 'Transcript'])) and (str(id)+'.mp3' in os.listdir('./audios')):
+        os.remove("./audios/"+str(id)+'.mp3')
+    print(70*"=")
+    print("Audios: {:.2f}%".format(100*i/470))
