1af0eeb641237acd549beae126fd93ae959b2d0e
alexandre prad <alexandre.pradeilles@gmail.com>
2023-03-22 14:23:01 +0100

    ADD update button

--- data_validate/data_validation.py
+++ data_validate/data_validation.py
@@ -11,6 +11,7 @@ body_list = data ["body"].values
 cat_list = data ["predicted_classe"].values
 
 def main():
+    st.title("Data annotation")
 
     # Note that page title/favicon are set in the __main__ clause below,
     # so they can also be set through the mega multipage app (see ../pandas_app.py).
@@ -27,10 +28,10 @@ def main():
          st.session_state['id'] = len(annotation)
          
     
+    st.markdown("""*:grey[Compteur : {}]*""".format(len(st.session_state['annotation'])))
     
-    
-    st.write(body_list[st.session_state.id])
-    st.markdown('**:red[{}]**'.format(cat_list[st.session_state.id]))
+    st.write(body_list[len(st.session_state['annotation'])])
+    st.markdown('**:red[{}]**'.format(cat_list[len(st.session_state['annotation'])]))
     
     col1, col2, col3, col4, col5 = st.columns(5)
 
@@ -55,6 +56,9 @@ def main():
             st.session_state['id']+=1
     
     with col5:
+        if st.button("Update"):
+            pass
+
         if st.button("Save"):
             np.save("annotations.npy",st.session_state.annotation , allow_pickle=True)
             st.write("**:red[You can now leave the session]**")
@@ -65,11 +69,13 @@ def main():
             
         if st.button("+10000"):
             st.session_state['id']+=10000
+
+    st.markdown("""*:grey[Compteur : {}]*""".format(len(st.session_state['annotation'])))
     
 ### INTRODUCTION ###
-st.title("Data annotation")
+# st.title("Data annotation")
 main()
-st.markdown("""*:grey[Compteur : {}]*""".format(len(st.session_state['annotation'])))
+# st.markdown("""*:grey[Compteur : {}]*""".format(len(st.session_state['annotation'])))
 
 
 
